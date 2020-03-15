d84b4fc98c27c111147294b6ef87b34173ee91ea
Eddy <eddy.nguyen@99designs.com>
2020-03-16 10:15:11 +1100

    Revert "Set up babel with jest"
    This reverts commit 3e698d42418200703095625eb72db9af573f7b7a.

--- src/generateUrl/generateUrl.test.ts
+++
@@ -1,9 +0,0 @@
-import generateUrl from './generateUrl';
-
-describe('generateUrl', () => {
-  it('should generate URL with no dynamic path or query string', () => {
-    const result = generateUrl('/app/login', {});
-
-    expect(result).toBe('/app/login');
-  });
-});
