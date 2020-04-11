d351158715c2b5b42fbd12978428648e4a4e1db9
Eddy Nguyen <eddeee.nguyen@gmail.com>
2020-04-11 14:25:03 +1000

    Add optional params to sample yml of NextJS

--- sample/output/seo/routes/about/useParamsAbout.ts
+++ sample/output/seo/routes/about/useParamsAbout.ts
@@ -3,6 +3,11 @@ import { PathParamsNextJSAbout } from './patternAbout';
 import { useRouter } from 'next/router';
 const useParamsAbout = (): PathParamsNextJSAbout => {
   const query = useRouter().query;
-  return { target: query.target as string, topic: query.topic as string };
+  return {
+    target: query.target as string,
+    topic: query.topic as string,
+    optional: query.optional ? query.optional : undefined,
+    optionalEnum: query.optionalEnum ? query.optionalEnum : undefined,
+  };
 };
 export default useParamsAbout;
