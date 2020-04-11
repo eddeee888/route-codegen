a1e8e6238bb4f15f70bcfa863ffa5b79137d640a
Eddy Nguyen <eddeee.nguyen@gmail.com>
2020-04-11 11:05:14 +1000

    Update test:sample to generate:sample

--- scripts/ci/check-sample.sh
+++ scripts/ci/check-sample.sh
@@ -1,10 +1,10 @@
 echo "*** Generate route modules in sample"
-yarn test:sample
+yarn generate:sample
 
 echo "*** Check if commited sample matches generated sample"
 if ! git diff --quiet  ; then
   echo "Looks like codegen needs to be run:"
   git --no-pager diff HEAD --color
-  echo "Have you tried running 'yarn test:sample'?"
+  echo "Have you tried running 'yarn generate:sample'?"
   exit 1
 fi
