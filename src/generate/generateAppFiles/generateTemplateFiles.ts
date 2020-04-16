2d92e707adef572ea4f2f6fd755e494b760505a8
Eddy Nguyen <eddeee.nguyen@gmail.com>
2020-04-16 23:01:53 +1000

    Turn if-else into a switch to make it easier to read

--- src/generate/generateAppFiles/generateTemplateFiles.ts
+++ src/generate/generateAppFiles/generateTemplateFiles.ts
@@ -48,77 +48,83 @@ const generateTemplateFiles: GenerateTemplateFiles = ({
   const files = [patternFile, genUrlFile];
 
   // Handle file generation for each routing type
-  if (routingType === RoutingType.ReactRouterV5) {
-    if (shouldGenerateLink) {
-      const linkFile = generatorReactRouterV5.generateLinkFile({
-        routeName,
-        destinationDir,
-        routeLinkOption: routeLinkOptions.ReactRouterV5,
-        patternNamedExports,
-        importGenerateUrl,
-      });
-      files.push(linkFile);
-    }
-    if (routeLinkOptions.ReactRouterV5.useParams && !!patternNamedExports.pathParamsInterfaceName) {
-      const useParamsFile = generatorReactRouterV5.generateUseParamsFile({
-        routeName,
-        destinationDir,
-        patternName: patternNamedExports.patternName,
-        pathParamsFilename: patternNamedExports.filename,
-        pathParamsInterfaceName: patternNamedExports.pathParamsInterfaceName,
-      });
-      files.push(useParamsFile);
-    }
-    if (routeLinkOptions.ReactRouterV5.useRedirect) {
-      const useRedirectFile = generatorReactRouterV5.generateUseRedirectFile({
-        routeName,
-        destinationDir,
-        patternNamedExports,
-        importGenerateUrl,
-      });
-      files.push(useRedirectFile);
-    }
-  } else if (routingType === RoutingType.NextJS) {
-    if (shouldGenerateLink) {
-      const linkFile = generatorNextJS.generateLinkFile({
-        routeName,
-        destinationDir,
-        routeLinkOption: routeLinkOptions.NextJS,
-        patternNamedExports,
-        importGenerateUrl,
-      });
-      files.push(linkFile);
-    }
-    if (routeLinkOptions.NextJS.useParams && !!patternNamedExports.pathParamsInterfaceNameNextJS) {
-      const useParamsFileNextJS = generatorNextJS.generateUseParamsFile({
-        routeName,
-        routePattern,
-        destinationDir,
-        pathParamsFilename: patternNamedExports.filename,
-        pathParamsInterfaceName: patternNamedExports.pathParamsInterfaceNameNextJS,
-      });
-      files.push(useParamsFileNextJS);
-    }
-  } else if (routingType === RoutingType.Default) {
-    if (shouldGenerateLink) {
-      const linkFile = generatorDefault.generateLinkFile({
-        routeName,
-        destinationDir,
-        routeLinkOption: routeLinkOptions.Default,
-        patternNamedExports,
-        importGenerateUrl,
-      });
-      files.push(linkFile);
-    }
-    if (routeLinkOptions.Default.useRedirect) {
-      const useRedirectDefault = generatorDefault.generateUseRedirectFile({
-        routeName,
-        importGenerateUrl,
-        destinationDir,
-        patternNamedExports,
-      });
-      files.push(useRedirectDefault);
-    }
+  switch (routingType) {
+    case RoutingType.ReactRouterV5:
+      if (shouldGenerateLink) {
+        const linkFile = generatorReactRouterV5.generateLinkFile({
+          routeName,
+          destinationDir,
+          routeLinkOption: routeLinkOptions.ReactRouterV5,
+          patternNamedExports,
+          importGenerateUrl,
+        });
+        files.push(linkFile);
+      }
+      if (routeLinkOptions.ReactRouterV5.useParams && !!patternNamedExports.pathParamsInterfaceName) {
+        const useParamsFile = generatorReactRouterV5.generateUseParamsFile({
+          routeName,
+          destinationDir,
+          patternName: patternNamedExports.patternName,
+          pathParamsFilename: patternNamedExports.filename,
+          pathParamsInterfaceName: patternNamedExports.pathParamsInterfaceName,
+        });
+        files.push(useParamsFile);
+      }
+      if (routeLinkOptions.ReactRouterV5.useRedirect) {
+        const useRedirectFile = generatorReactRouterV5.generateUseRedirectFile({
+          routeName,
+          destinationDir,
+          patternNamedExports,
+          importGenerateUrl,
+        });
+        files.push(useRedirectFile);
+      }
+      break;
+
+    case RoutingType.NextJS:
+      if (shouldGenerateLink) {
+        const linkFile = generatorNextJS.generateLinkFile({
+          routeName,
+          destinationDir,
+          routeLinkOption: routeLinkOptions.NextJS,
+          patternNamedExports,
+          importGenerateUrl,
+        });
+        files.push(linkFile);
+      }
+      if (routeLinkOptions.NextJS.useParams && !!patternNamedExports.pathParamsInterfaceNameNextJS) {
+        const useParamsFileNextJS = generatorNextJS.generateUseParamsFile({
+          routeName,
+          routePattern,
+          destinationDir,
+          pathParamsFilename: patternNamedExports.filename,
+          pathParamsInterfaceName: patternNamedExports.pathParamsInterfaceNameNextJS,
+        });
+        files.push(useParamsFileNextJS);
+      }
+      break;
+
+    case RoutingType.Default:
+      if (shouldGenerateLink) {
+        const linkFile = generatorDefault.generateLinkFile({
+          routeName,
+          destinationDir,
+          routeLinkOption: routeLinkOptions.Default,
+          patternNamedExports,
+          importGenerateUrl,
+        });
+        files.push(linkFile);
+      }
+      if (routeLinkOptions.Default.useRedirect) {
+        const useRedirectDefault = generatorDefault.generateUseRedirectFile({
+          routeName,
+          importGenerateUrl,
+          destinationDir,
+          patternNamedExports,
+        });
+        files.push(useRedirectDefault);
+      }
+      break;
   }
 
   return files;
