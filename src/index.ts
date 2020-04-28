0ad76ecaebafcbda1bf3ddb3f64aacec9a907b22
Eddy Nguyen <eddeee.nguyen@gmail.com>
2020-04-28 23:06:57 +1000

    Make sure route-codegen/generateUrl and route-codegen/RedirectServerSide works

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { default as generateUrl } from "./generateUrl";
-export { default as RedirectServerSide } from "./RedirectServerSide";
+export { default as generateUrl } from "./utils/generateUrl";
+export * from "./utils/generateUrl";
+export { default as RedirectServerSide } from "./utils/RedirectServerSide";
+export * from "./utils/RedirectServerSide";
