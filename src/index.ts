8ddea38362f593e203baa3f7e323adf67d2f4a33
Eddy Nguyen <eddeee.nguyen@gmail.com>
2020-04-29 00:34:47 +1000

    Fix root exports (#45)

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as generateUrl } from "./generateUrl";
-export * from "./generateUrl";
-export { default as RedirectServerSide } from "./RedirectServerSide";
-export * from "./RedirectServerSide";
