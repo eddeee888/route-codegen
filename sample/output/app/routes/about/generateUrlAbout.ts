cf879c61b831bc0d5333f869b056a40c3d50e21e
Eddy Nguyen <eddeee.nguyen@gmail.com>
2020-05-08 19:41:13 +1000

    Update `generateUrl` templates to have non-required urlParts if no path params (#52)
    ## Template
    
    - `generateUrl` templates to have non-required urlParts if no path params

--- sample/output/app/routes/about/generateUrlAbout.ts
+++ sample/output/app/routes/about/generateUrlAbout.ts
@@ -1,5 +1,5 @@
 /* This file was automatically generated with route-codegen and should not be edited. */
 import generateUrl from "route-codegen/generateUrl";
 import { patternAbout, UrlPartsAbout } from "./patternAbout";
-const generateUrlAbout = (urlParts: UrlPartsAbout): string => generateUrl(patternAbout, urlParts.path, urlParts.urlQuery);
+const generateUrlAbout = (urlParts: UrlPartsAbout): string => generateUrl(patternAbout, urlParts.path, urlParts?.urlQuery);
 export default generateUrlAbout;
--- sample/output/auth/routes/legacy/generateUrlLegacy.ts
+++ sample/output/auth/routes/legacy/generateUrlLegacy.ts
@@ -1,5 +1,5 @@
 /* This file was automatically generated with route-codegen and should not be edited. */
 import generateUrl from "route-codegen/generateUrl";
 import { patternLegacy, UrlPartsLegacy } from "./patternLegacy";
-const generateUrlLegacy = (urlParts: UrlPartsLegacy): string => generateUrl(patternLegacy, {}, urlParts.urlQuery);
+const generateUrlLegacy = (urlParts?: UrlPartsLegacy): string => generateUrl(patternLegacy, {}, urlParts?.urlQuery);
 export default generateUrlLegacy;
