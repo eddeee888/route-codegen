9ed38ec892aad83195dfdf56e2839825883b36e5
Eddy Nguyen <eddeee.nguyen@gmail.com>
2020-06-03 22:43:12 +1000

    Generate samples for with-origins

--- sample/output-with-origins/app/routes/graphql/patternGraphql.ts
+++ sample/output-with-origins/app/routes/graphql/patternGraphql.ts
@@ -1,6 +1,6 @@
 /* This file was automatically generated with route-codegen and should not be edited. */
 export const patternGraphql = "/graphql";
-export const originGraphql = "https://app.domain.com";
+export const originGraphql = "https://api.domain.com";
 
 export interface UrlPartsGraphql {
   urlQuery?: Record<string, string>;
