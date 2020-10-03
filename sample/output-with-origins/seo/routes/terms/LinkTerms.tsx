1522252f9f58789cc1f1ebc99bda19d77cb25c69
Eddy Nguyen <eddeee.nguyen@gmail.com>
2020-10-03 20:57:15 +1000

    Fix problem of path being desctructured when there is none

--- sample/output-with-origins/seo/routes/terms/LinkTerms.tsx
+++ sample/output-with-origins/seo/routes/terms/LinkTerms.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import Link, { LinkProps } from "next/link";
 import { UrlPartsTerms, patternNextJSTerms } from "./patternTerms";
 type LinkTermsProps = Omit<LinkProps, "href"> & UrlPartsTerms;
-const LinkTerms: React.FunctionComponent<LinkTermsProps> = ({ path = {}, urlQuery = {}, ...props }) => {
+const LinkTerms: React.FunctionComponent<LinkTermsProps> = (props) => {
+  const { urlQuery = {}, ...rest } = props;
+  const path = {};
   const pathname = patternNextJSTerms;
   const nextHref = {
     pathname: pathname,
@@ -12,6 +14,6 @@ const LinkTerms: React.FunctionComponent<LinkTermsProps> = ({ path = {}, urlQuer
       ...urlQuery,
     },
   };
-  return <Link {...props} href={nextHref} />;
+  return <Link {...rest} href={nextHref} />;
 };
 export default LinkTerms;
--- sample/output/seo/routes/about/LinkAbout.tsx
+++ sample/output/seo/routes/about/LinkAbout.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import Link, { LinkProps } from "next/link";
 import { UrlPartsAbout, patternNextJSAbout, possilePathParamsAbout } from "./patternAbout";
 type LinkAboutProps = Omit<LinkProps, "href"> & UrlPartsAbout;
-const LinkAbout: React.FunctionComponent<LinkAboutProps> = ({ path = {}, urlQuery = {}, ...props }) => {
+const LinkAbout: React.FunctionComponent<LinkAboutProps> = (props) => {
+  const { path = {}, urlQuery = {}, ...rest } = props;
   const pathname = possilePathParamsAbout
     .filter((key) => !(key in path))
     .reduce((prevPattern, suppliedParam) => prevPattern.replace(`/[${suppliedParam}]`, ""), patternNextJSAbout);
@@ -14,6 +15,6 @@ const LinkAbout: React.FunctionComponent<LinkAboutProps> = ({ path = {}, urlQuer
       ...urlQuery,
     },
   };
-  return <Link {...props} href={nextHref} />;
+  return <Link {...rest} href={nextHref} />;
 };
 export default LinkAbout;
