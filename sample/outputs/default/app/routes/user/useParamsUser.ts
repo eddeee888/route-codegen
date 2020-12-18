d5d28d779d8e0b5818079c9b0314282619f5c75c
Eddy Nguyen <eddeee.nguyen@gmail.com>
2020-12-18 23:22:06 +1100

    Update samples with strict and loose modes

--- sample/outputs/default/app/routes/user/useParamsUser.ts
+++ sample/outputs/default/app/routes/user/useParamsUser.ts
@@ -2,11 +2,6 @@
 import { PathParamsUser, patternUser as pattern } from "./patternUser";
 import { useRouteMatch } from "react-router";
 const useParamsUser = (): PathParamsUser => {
-  const { path, params } = useRouteMatch<PathParamsUser>();
-  if (path !== pattern) {
-    const error = `You are trying to use useParams for "${pattern}" in "${path}". Make sure you are using the right route link object!`;
-    throw new Error(error);
-  }
-  return params;
+  return useRouteMatch<PathParamsUser>().params;
 };
 export default useParamsUser;
