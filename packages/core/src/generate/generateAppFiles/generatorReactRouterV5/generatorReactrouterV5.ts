dbcc6cc2b4d54b1b1057c8ca04b77321bb453328
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-04-21 23:02:35 +1000

    Update generatorRR5.generateUseParams to handle named exports and generateUrl param obj

--- packages/core/src/generate/generateAppFiles/generatorReactRouterV5/generatorReactrouterV5.ts
+++ packages/core/src/generate/generateAppFiles/generatorReactRouterV5/generatorReactrouterV5.ts
@@ -1,10 +1,10 @@
-import { default as generateUseParamsFile } from "./generateUseParamsFileReactRouterV5";
+import { generateUseParamsFileReactRouterV5 } from "./generateUseParamsFileReactRouterV5";
 import { default as generateUseRedirectFile } from "./generateUseRedirectReactRouterV5";
 import { generateLinkFileReactRouterV5 } from "./generateLinkFileReactRouterV5";
 import { generateRedirectFileReactRouterV5 } from "./generateRedirectFileReactRouterV5";
 
 export const generatorReactRouterV5 = {
-  generateUseParamsFile,
+  generateUseParamsFile: generateUseParamsFileReactRouterV5,
   generateUseRedirectFile,
   generateLinkFile: generateLinkFileReactRouterV5,
   generateRedirectFile: generateRedirectFileReactRouterV5,
