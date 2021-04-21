c94a2d3043be3b1a6d34deefa289212e680c8c52
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-04-21 23:22:54 +1000

    Update utils/generateUrl to split pattern from UrlParams

--- packages/utils/src/generateUrl/generateUrl.test.ts
+++ packages/utils/src/generateUrl/generateUrl.test.ts
@@ -3,88 +3,88 @@ import { generateUrl } from "./generateUrl";
 describe("generateUrl", () => {
   describe("no path or query string", () => {
     it("should generate URL with no dynamic path params or query string", () => {
-      const result = generateUrl({ pattern: "/app/login", path: {} });
+      const result = generateUrl("/app/login", { path: {} });
       expect(result).toBe("/app/login");
     });
   });
 
   describe("paths - strings", () => {
     it("should generate URL with required path param", () => {
-      const result = generateUrl({ pattern: "/app/users/:id", path: { id: "onehundo" } });
+      const result = generateUrl("/app/users/:id", { path: { id: "onehundo" } });
       expect(result).toBe("/app/users/onehundo");
     });
 
     it("should throw error if path param is required but not in path object", () => {
-      expect(() => generateUrl({ pattern: "/app/users/:id", path: {} })).toThrowError('Expected "id" to be a string');
-      expect(() => generateUrl({ pattern: "/app/users/:id", path: { name: "Hundo" } })).toThrowError('Expected "id" to be a string');
+      expect(() => generateUrl("/app/users/:id", { path: {} })).toThrowError('Expected "id" to be a string');
+      expect(() => generateUrl("/app/users/:id", { path: { name: "Hundo" } })).toThrowError('Expected "id" to be a string');
     });
 
     it("should generate URL if path param is not required and not in path object", () => {
-      const result = generateUrl({ pattern: "/app/users/:id?", path: {} });
+      const result = generateUrl("/app/users/:id?", { path: {} });
       expect(result).toBe("/app/users");
     });
 
     it("should generate URL if path param is not required but path is in path object", () => {
-      const result = generateUrl({ pattern: "/app/users/:id?", path: { id: "onehundo" } });
+      const result = generateUrl("/app/users/:id?", { path: { id: "onehundo" } });
       expect(result).toBe("/app/users/onehundo");
     });
 
     it("should generate URL with required path param", () => {
-      const result = generateUrl({ pattern: "/app/users/:id", path: { id: "onehundo" } });
+      const result = generateUrl("/app/users/:id", { path: { id: "onehundo" } });
       expect(result).toBe("/app/users/onehundo");
     });
 
     it("should throw error if path param is required but not in path object", () => {
-      expect(() => generateUrl({ pattern: "/app/users/:id", path: {} })).toThrowError('Expected "id" to be a string');
-      expect(() => generateUrl({ pattern: "/app/users/:id", path: { name: "Hundo" } })).toThrowError('Expected "id" to be a string');
+      expect(() => generateUrl("/app/users/:id", { path: {} })).toThrowError('Expected "id" to be a string');
+      expect(() => generateUrl("/app/users/:id", { path: { name: "Hundo" } })).toThrowError('Expected "id" to be a string');
     });
 
     it("should generate URL if path param is not required and not in path object", () => {
-      const result = generateUrl({ pattern: "/app/users/:id?", path: {} });
+      const result = generateUrl("/app/users/:id?", { path: {} });
       expect(result).toBe("/app/users");
     });
 
     it("should generate URL if path param is not required but path is in path object", () => {
-      const result = generateUrl({ pattern: "/app/users/:id?", path: { id: "onehundo" } });
+      const result = generateUrl("/app/users/:id?", { path: { id: "onehundo" } });
       expect(result).toBe("/app/users/onehundo");
     });
   });
 
   describe("paths - enums", () => {
     it("should generate URL with required enum path param", () => {
-      const result = generateUrl({ pattern: "/app/users/:subview(profile|pictures)", path: { subview: "profile" } });
+      const result = generateUrl("/app/users/:subview(profile|pictures)", { path: { subview: "profile" } });
       expect(result).toBe("/app/users/profile");
     });
 
     it("should throw error with required enum path param and path is not in path object", () => {
-      expect(() => generateUrl({ pattern: "/app/users/:subview(profile|pictures)", path: { house: "profile" } })).toThrowError(
+      expect(() => generateUrl("/app/users/:subview(profile|pictures)", { path: { house: "profile" } })).toThrowError(
         'Expected "subview" to be a string'
       );
     });
 
     it("should throw error with required enum path param and path is not part of enums", () => {
-      expect(() => generateUrl({ pattern: "/app/users/:subview(profile|pictures)", path: { subview: "dog" } })).toThrowError(
+      expect(() => generateUrl("/app/users/:subview(profile|pictures)", { path: { subview: "dog" } })).toThrowError(
         'Expected "subview" to match "profile|pictures", but got "dog"'
       );
     });
 
     it("should generate URL with optional enum path param and path not in path object", () => {
-      const result = generateUrl({ pattern: "/app/users/:subview(profile|pictures)?", path: {} });
+      const result = generateUrl("/app/users/:subview(profile|pictures)?", { path: {} });
       expect(result).toBe("/app/users");
     });
 
     it("should generate URL with optional enum path param and path in path object", () => {
-      const result = generateUrl({ pattern: "/app/users/:subview(profile|pictures)?", path: { subview: "profile" } });
+      const result = generateUrl("/app/users/:subview(profile|pictures)?", { path: { subview: "profile" } });
       expect(result).toBe("/app/users/profile");
     });
 
     it("should generate URL with optional enum path param and path is not in path object", () => {
-      const result = generateUrl({ pattern: "/app/users/:subview(profile|pictures)?", path: { house: "profile" } });
+      const result = generateUrl("/app/users/:subview(profile|pictures)?", { path: { house: "profile" } });
       expect(result).toBe("/app/users");
     });
 
     it("should throw error with optional enum path param and path is not part of enums", () => {
-      expect(() => generateUrl({ pattern: "/app/users/:subview(profile|pictures)?", path: { subview: "dog" } })).toThrowError(
+      expect(() => generateUrl("/app/users/:subview(profile|pictures)?", { path: { subview: "dog" } })).toThrowError(
         'Expected "subview" to match "profile|pictures", but got "dog"'
       );
     });
@@ -92,18 +92,17 @@ describe("generateUrl", () => {
 
   describe("query strings", () => {
     it("should generate one query string correctly", () => {
-      const result = generateUrl({ pattern: "/app/users/:id", path: { id: "oneHundo" }, query: { from: "homepage" } });
+      const result = generateUrl("/app/users/:id", { path: { id: "oneHundo" }, query: { from: "homepage" } });
       expect(result).toBe("/app/users/oneHundo?from=homepage");
     });
 
     it("should generate multiple query strings correctly", () => {
-      const result = generateUrl({ pattern: "/app/users/:id", path: { id: "oneHundo" }, query: { from: "homepage", to: "login" } });
+      const result = generateUrl("/app/users/:id", { path: { id: "oneHundo" }, query: { from: "homepage", to: "login" } });
       expect(result).toBe("/app/users/oneHundo?from=homepage&to=login");
     });
 
     it("should encode special characters correctly", () => {
-      const result = generateUrl({
-        pattern: "/app/users/:id",
+      const result = generateUrl("/app/users/:id", {
         path: { id: "oneHundo" },
         query: { from: "/homepage", redirect: "https://domain.com/login?from=/home" },
       });
@@ -111,8 +110,7 @@ describe("generateUrl", () => {
     });
 
     it("should generate 1 undefined query string correctly", () => {
-      const result = generateUrl({
-        pattern: "/app/users/:id",
+      const result = generateUrl("/app/users/:id", {
         path: { id: "oneHundo" },
         query: { from: undefined },
       });
@@ -120,8 +118,7 @@ describe("generateUrl", () => {
     });
 
     it("should generate more than 1 undefined query string correctly", () => {
-      const result = generateUrl({
-        pattern: "/app/users/:id",
+      const result = generateUrl("/app/users/:id", {
         path: { id: "oneHundo" },
         query: { from: undefined, redirect: undefined },
       });
@@ -129,8 +126,7 @@ describe("generateUrl", () => {
     });
 
     it("should generate mixed undefined and string correctly", () => {
-      const result = generateUrl({
-        pattern: "/app/users/:id",
+      const result = generateUrl("/app/users/:id", {
         path: { id: "oneHundo" },
         query: { from: "homepage", redirect: undefined, to: "test" },
       });
@@ -140,8 +136,7 @@ describe("generateUrl", () => {
 
   describe("with origin", () => {
     it("should generate url with origin", () => {
-      const result = generateUrl({
-        pattern: "/app/users/:id",
+      const result = generateUrl("/app/users/:id", {
         path: { id: "oneHundo" },
         query: { from: "homepage" },
         origin: "https://test.domain",
