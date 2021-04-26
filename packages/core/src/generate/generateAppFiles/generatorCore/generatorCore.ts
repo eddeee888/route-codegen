681455b7d65ee540915de5d99a45c94cd18414da
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-04-26 22:02:42 +1000

    Remove generatorCore

--- packages/core/src/generate/generateAppFiles/generatorCore/generatorCore.ts
+++
@@ -1,7 +0,0 @@
-import { default as generatePatternFile } from "./generatePatternFile";
-import { generateUrlFile } from "./generateUrlFile";
-
-export const generatorCore = {
-  generatePatternFile,
-  generateUrlFile,
-};
