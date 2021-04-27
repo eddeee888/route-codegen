d3f928a567cf7ca1f0e98bda51bef6e6bc351f49
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-04-27 21:27:40 +1000

    Add pluginConfigHelpers to easily handle plugin config

---
+++ packages/core/src/utils/pluginConfigHelpers/pluginConfigHelpers.ts
@@ -0,0 +1,32 @@
+/**
+ * Raw plugin config from reading yml
+ */
+export interface RawPluginConfig {
+  name: string;
+  type?: string;
+  config?: Record<string, unknown>;
+}
+
+export interface ParsedPluginConfig {
+  name: string;
+  type: PluginConfigType;
+  config?: Record<string, unknown>;
+}
+
+type PluginConfigType = "pattern" | "route";
+
+const parse = (plugins: RawPluginConfig[]): ParsedPluginConfig[] => {
+  return plugins.map((plugin) => {
+    return {
+      name: plugin.name,
+      type: plugin.type === "route" ? "route" : "pattern",
+      config: plugin.config,
+    };
+  });
+};
+
+const findFirstOfType = (plugins: ParsedPluginConfig[], type: PluginConfigType): ParsedPluginConfig | undefined => {
+  return plugins.find((plugin) => plugin.type === type);
+};
+
+export const pluginConfigHelpers = { findFirstOfType, parse };
