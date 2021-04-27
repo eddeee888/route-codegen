32c69aa1d0a0c0b8f112dffc42cb69710e92451d
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-04-27 22:26:19 +1000

    Update BasePlugin to handle linkOptions

--- packages/core/src/utils/BasePlugin/BasePlugin.ts
+++ packages/core/src/utils/BasePlugin/BasePlugin.ts
@@ -1,13 +1,36 @@
 import { throwError } from "../throwError";
+import { Import, LinkOptions, PatternNamedExports, TemplateFile, TopLevelGenerateOptions } from "../types";
 
-export class BasePlugin<C, R> {
+export interface BasePluginConfig {
+  appName: string;
+  routeName: string;
+  routePattern: string;
+  routeLinkOptions: LinkOptions;
+  topLevelGenerateOptions: TopLevelGenerateOptions;
+  destinationDir: string;
+  patternNamedExports: PatternNamedExports;
+  importGenerateUrl: Import;
+  importRedirectServerSide: Import;
+}
+
+export class BasePlugin<L = any, C = BasePluginConfig, R = TemplateFile[]> {
   config: C;
+  linkOptions: L | null = null;
 
   constructor(config: C) {
     this.config = config;
+    this._parseLinkOptions();
   }
 
   generate(): R {
     return throwError([], "Implement generate function");
+  }
+
+  protected _parseLinkOptions(): void {
+    return throwError([], "Implement _parseLinkOptions function");
+  }
+
+  protected _getLinkOptions(): L {
+    return throwError([], "Implement _getLinkOptions function");
   }
 }
