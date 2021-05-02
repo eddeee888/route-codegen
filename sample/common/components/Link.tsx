6c7455d112cf1199f4d720e8b3cbc3b326a6fcae
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-05-02 11:22:56 +1000

    Update sample RRLink implementation

--- sample/common/components/Link.tsx
+++ sample/common/components/Link.tsx
@@ -1,11 +1,12 @@
 import React, { FunctionComponent } from "react";
+import { Link as RRLink, LinkProps as RRLinkProps } from "react-router-dom";
 
-export interface LinkProps {
+export interface LinkProps extends Omit<RRLinkProps, "to"> {
   href: string;
 }
 
-const Link: FunctionComponent<LinkProps> = (props) => {
-  return <a {...props} />;
+const Link: FunctionComponent<LinkProps> = ({ href, ...props }) => {
+  return <RRLink {...props} to={href} />;
 };
 
 export default Link;
